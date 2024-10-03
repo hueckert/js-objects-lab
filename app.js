392e5ddd144e45962920321347adfd0e8896cb9b
howard <howard.ueckert@protonmail.com>
2024-10-02 22:52:16 -0500

    exercise 3

--- app.js
+++ app.js
@@ -23,3 +23,28 @@ const game = {
   
   console.log(game)
 
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+game.difficulty = "Med"
+//testing
+console.log(game)
+
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+game.party.push(pokemon[3], pokemon[6], pokemon[18])
+
+console.log(game.party)
